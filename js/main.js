82304413d68d1a83ae680e740d75bb3989e5eaaa
Bavri <bavrin1999@mail.ru>
2022-02-11 15:19:59 +0300

    add slider

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+
+  loop: true,
+
+  // If we need pagination
+  
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+
+  // And if we need scrollbar
+  
+});
