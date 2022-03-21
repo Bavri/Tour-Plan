8ea52a26ce024ee7f772186accbb24c392987c13
Bavri <bavrin1999@mail.ru>
2022-03-21 10:18:50 +0300

    add final

--- send.php
+++ send.php
@@ -4,21 +4,30 @@ require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
-if (isset($_POST['message'])){
+
 // Переменные, которые отправляет пользователь
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
+$email = $_POST['email'];
 
 
 // Формирование самого письма
-$title = "Best Tour Plan новое обращение";
-$body = "
-<h2>Новое письмо</h2>
-<b>Имя:</b> $name<br>
-<b>Номер:</b> $phone<br><br>
-<b>Сообщение:</b><br>$message
-";
+if (empty($email)) {
+    $title = "Best Tour Plan новое обращение";
+    $body = "
+    <h2>Новое письмо</h2>
+    <b>Имя:</b> $name<br>
+    <b>Номер:</b> $phone<br><br>
+    <b>Сообщение:</b><br>$message
+    ";
+} else {
+    $title = "Подписка на рассылку Best Tour Plan";
+    $body = "
+    <h2>Новое письмо</h2>
+    <b>Подписка оформлена</b>
+    ";
+}
 
 // Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
@@ -57,54 +66,13 @@ else {$result = "error";}
 }
 
 // Отображение результата
-header('Location: thank-you-appeal.html');
-
-} else if (isset($_POST['subscribe']) {
-$email = $_POST['email'];
-// Формирование самого письма   
-$title = "Best Tour Plan новый сабскрабер";
-$body = "
-<h2>Новое письмо</h2>
-<b>Адрес:</b> $email<br>
-";
-
-// Настройки PHPMailer
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-try {
-    $mail->isSMTP();   
-    $mail->CharSet = "UTF-8";
-    $mail->SMTPAuth   = true;
-    //$mail->SMTPDebug = 2;
-    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
-
-    // Настройки вашей почты
-    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-    $mail->Username   = 'thejove3101@gmail.com'; // Логин на почте
-    $mail->Password   = 'Protocol1Zero'; // Пароль на почте
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port       = 465;
-    $mail->setFrom('thejove3101@gmail.com', 'Олег Сомов'); // Адрес самой почты и имя отправителя
-
-    // Получатель письма
-    $mail->addAddress('bavrin1999@mail.ru');  
-   
-
-
-// Отправка сообщения
-$mail->isHTML(true);
-$mail->Subject = $title;
-$mail->Body = $body;    
-
-// Проверяем отравленность сообщения
-if ($mail->send()) {$result = "success";} 
-else {$result = "error";}
-
-} catch (Exception $e) {
-    $result = "error";
-    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+if (empty($email)) {
+    header('Location: thank-you-appeal.html');
+} else {
+    header('Location: subscribe.html');
 }
 
-// Отображение результата
-header('Location: subscribe.html');
 
-}
+
+
+
