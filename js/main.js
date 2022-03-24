2fe34e8d8f9cf408cd78d86b7b05957a6fd51cbe
Bavri <bavrin1999@mail.ru>
2022-03-24 15:02:33 +0300

    add

--- js/main.js
+++ js/main.js
@@ -110,4 +110,55 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+
+
+
+
 }); // end ready
+
+$('[name=phone]').mask("+7 (999) 999-99-99");
+
+$(".form").each(function() {
+    $(this).validate({
+        element: "form",
+        validClass: "input__required",
+        errorClass: "invalid",
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            phone: {
+                required: true,
+                minlength: 18
+            },
+            message: {
+                required: true
+            }
+        },
+        messages: {
+            name: {
+                required: "* Enter your full name",
+                minlength: "* More than 2 letters long"
+            },
+            email: {
+                required: "* Enter your email address",
+                email: "* Must be name@domain.com"
+            },
+            phone: {
+                required: "* Enter your phone number",
+                minlength: "* More than 18 letters long",
+            }
+        },
+        highlight: function(element, errorClass, validClass) {
+            $(element).addClass(errorClass).removeClass(validClass);
+        },
+        unhighlight: function(element, errorClass, validClass) {
+            $(element).removeClass(errorClass).addClass(validClass);
+        }
+    });
+});
